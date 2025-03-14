d86d805be70d0e4c1cecefa44c2a312ceb0454a2
MarkusFuchs68 <markus_fuchs@gmx.net>
2025-03-14 14:57:31 +0100

    added experimenting notebook for feature_map_extraction

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -7,6 +7,10 @@ import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from imblearn.metrics import classification_report_imbalanced
 
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D 
+
 
 def plot_learning_curve(history_model, loss='loss', metric='accuracy'):
     plt.figure(figsize=(12, 4))
@@ -88,14 +92,14 @@ def report_model_performance(y_true, y_pred, class_names):
 # Function to extract model summary as a DataFrame
 def model_summary_to_df(model):
     import io
-    
+
     # Redirect sys.stdout to capture model.summary() output
     stream = io.StringIO()
-    
+
     # Redirect Keras summary output to a variable
     model.summary(print_fn=lambda x: stream.write(x + "\n"))
     summary_str = stream.getvalue()
-    
+
     # Parse summary output
     lines = summary_str.split("\n")
     data = []
@@ -106,8 +110,68 @@ def model_summary_to_df(model):
             output_shape = parts[1]
             param_count = parts[-1]
             data.append([layer_nametype, output_shape, param_count])
-    
+
     # Create DataFrame
     df = pd.DataFrame(data, columns=["Layer Name (type)", "Output Shape", "Param Count"])
     return df
 
+
+def show_feature_maps(model, image):
+    """
+    Displays how the input image (Shape: (224, 224, 3), pixel-value-range 0-255),
+    makes its way through the convolutional layers
+    """
+    # Retrieve the names of all (even recursive) convolution layers in the model
+    def get_conv_layers(model):
+        inner_conv_layers = []
+        for layer in model.layers:
+            if isinstance(layer, Conv2D):
+                inner_conv_layers.append((model,layer.name))
+            elif isinstance(layer, Model):
+                inner_conv_layers.extend(get_conv_layers(layer))
+        return inner_conv_layers
+
+    conv_layers = get_conv_layers(model)
+
+    # Loop through all convolution layers
+    for j, (inner_model,layer) in enumerate(conv_layers):
+
+        # Create a new model with the same input as the original model but with the output
+        # of the specific convolution layer
+        conv_model = Model(inputs=inner_model.input, outputs=inner_model.get_layer(layer).output)
+
+        # Add an extra dimension to the image to make it compatible with the batch (shape: (1, H, W, C))
+        image_batch = tf.expand_dims(image, axis=0)
+
+        # Normalize (optional, depends on model)
+        image_batch = image_batch / 255.0  # Normalize to [0,1]
+
+        # Predict the feature maps for the given image using the created model
+        feature_maps = conv_model.predict(image_batch, verbose=0)
+
+        # Squeeze to remove unnecessary dimensions, resulting in an array of shape (H, W, N)
+        feature_maps = tf.squeeze(feature_maps)
+
+        # Initialize a figure to display the feature maps
+        plt.figure(figsize=(12, 12))
+
+        # Loop through all feature maps of the layer
+        for i in range(feature_maps.shape[-1]):
+
+            # Calculate the number of subplots needed to display all feature maps
+            nb_subplot = feature_maps.shape[-1]**(1/2)
+
+            # If the number of subplots is not an integer, round up to the next integer
+            if nb_subplot - int(nb_subplot) != 0:
+                nb_subplot = int(nb_subplot) + 1
+            else: 
+                nb_subplot = int(nb_subplot)
+
+            # Create a subplot for each feature map
+            plt.subplot(nb_subplot, nb_subplot, i + 1)
+            plt.imshow(feature_maps[..., i])  # Display the feature map
+            plt.axis("off")  # Turn off the axes
+            plt.title(f'Output {layer} filter {i+1}', fontsize=16 - nb_subplot - 1)  # Add a title
+
+    # Display the results
+    plt.show()
