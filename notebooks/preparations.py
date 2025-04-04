8e70f621d6d1911834ace48a3a4fc2b822ee425c
MarkusFuchs68 <markus_fuchs@gmx.net>
2025-04-04 20:34:00 +0200

    changed to more robust copy

--- notebooks/preparations.py
+++ notebooks/preparations.py
@@ -10,7 +10,6 @@ def download_and_move_dataset():
     # Create directories if they do not exist
     if os.path.exists(dest_path):
         print("Dataset already exists.")
-        return True
     else:
         print("Dataset not found. Downloading...")
 
@@ -18,5 +17,5 @@ def download_and_move_dataset():
     path = kagglehub.dataset_download(handle="tawsifurrahman/covid19-radiography-database", force_download=True)
 
     # Move dataset to parent directory
-    shutil.move(os.path.join(path, COVID_FOLDER), "..")
+    shutil.copy(os.path.join(path, COVID_FOLDER), "..")
     print("Covid-19 Radiography Dataset downloaded and moved to parent directory.")
