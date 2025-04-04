4f3c089b2972e5929ce172114f5dcdcd4453d8b8
robert <kasparrobert@gmail.com>
2025-04-04 09:43:52 +0000

    added path definition

--- notebooks/preparations.py
+++ notebooks/preparations.py
@@ -2,16 +2,18 @@ import os
 import shutil
 import kagglehub
 
+COVID_FOLDER_PATH = "COVID-19_Radiography_Dataset"
+
 def download_and_move_dataset():
     # Create directories if they do not exist
-    if os.path.exists("COVID-19_Radiography_Dataset"):
+    if os.path.exists(COVID_FOLDER_PATH):
         print("Dataset already exists.")
         return True
     else:
         print("Dataset not found.")
-    os.makedirs("COVID-19_Radiography_Dataset", exist_ok=True)
+    os.makedirs(COVID_FOLDER_PATH, exist_ok=True)
     # Download dataset
     path = kagglehub.dataset_download(handle="tawsifurrahman/covid19-radiography-database", force_download=True)
     # Move dataset to parent directory
-    shutil.move(os.path.join(path, 'COVID-19_Radiography_Dataset'), "..")
+    shutil.move(os.path.join(path, COVID_FOLDER_PATH), "..")
     print("Covid-19 Radiography Dataset downloaded and moved to parent directory.")
